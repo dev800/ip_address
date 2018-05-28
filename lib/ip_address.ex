fba8138878f12265bcc20457fb3c8cfa12bbf651
WangJinzhong <andywang7259@163.com>
2018-05-28 09:49:09 +0800

    from github

--- lib/ip_address.ex
+++ lib/ip_address.ex
@@ -1,11 +1,13 @@
 defmodule IpAddress do
   def find(nil), do: {:error, :null}
   def find(""), do: {:error, :empty}
+
   def find(ip_string) when is_binary(ip_string) do
     IpAddress.Monipdb.find(ip_string)
   end
 
   def find!(nil), do: []
+
   def find!(ip_string) when is_binary(ip_string) do
     IpAddress.Monipdb.find!(ip_string)
   end
--- lib/ip_address/monipdb.ex
+++ lib/ip_address/monipdb.ex
@@ -2,18 +2,23 @@ defmodule IpAddress.Monipdb do
   def find(ip_string) when is_binary(ip_string) do
     try do
       case impl_find_from_erl(ip_string) do
-        {:ok, address_string} -> 
-          addresses = String.split(address_string, "\t") |> Enum.filter(fn(str) ->
-            String.length(str) > 0
-          end) |> Enum.uniq
+        {:ok, address_string} ->
+          addresses =
+            String.split(address_string, "\t")
+            |> Enum.filter(fn str ->
+              String.length(str) > 0
+            end)
+            |> Enum.uniq()
 
           {:ok, addresses}
+
         _ ->
           {:error, :not_found}
       end
     rescue
       _ in [MatchError] ->
         {:error, :nxdomain}
+
       true ->
         {:error, :nxdomain}
     end
@@ -27,6 +32,9 @@ defmodule IpAddress.Monipdb do
   end
 
   defp impl_find_from_erl(ip_string) when is_binary(ip_string) do
-    :ip_address_erl.find(String.to_charlist(ip_string), Path.absname("17monipdb.dat", Application.app_dir(:ip_address_erl, "priv")))
+    :ip_address_erl.find(
+      String.to_charlist(ip_string),
+      Path.absname("17monipdb.dat", Application.app_dir(:ip_address_erl, "priv"))
+    )
   end
 end
