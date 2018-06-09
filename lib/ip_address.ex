fce195b1b459a1802448d5b08b01dfaaed38fc86
WangJinzhong <andywang7259@163.com>
2018-06-09 08:10:25 +0800

    find by list

--- lib/ip_address.ex
+++ lib/ip_address.ex
@@ -6,9 +6,26 @@ defmodule IpAddress do
     IpAddress.Monipdb.find(ip_string)
   end
 
+  def find(ip_strings) when is_list(ip_strings) do
+    ip_strings
+    |> Enum.uniq
+    |> Enum.map(fn ip_string ->
+      {ip_string, find(ip_string)}
+    end)
+  end
+
   def find!(nil), do: []
 
   def find!(ip_string) when is_binary(ip_string) do
     IpAddress.Monipdb.find!(ip_string)
   end
+
+  def find!(ip_strings) when is_list(ip_strings) do
+    ip_strings
+    |> Enum.uniq
+    |> Enum.map(fn ip_string ->
+      {ip_string, find!(ip_string)}
+    end)
+  end
+
 end
